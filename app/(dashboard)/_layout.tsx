3c983ae5ca045bfc81d7154f99c544244b14f812
Lakhini Voshadee Abeysundara <lakhinivoshadee24@gmail.com>
2025-08-15 11:40:46 +0530

    design

--- app/(dashboard)/_layout.tsx
+++ app/(dashboard)/_layout.tsx
@@ -2,47 +2,61 @@ import AntDesign from "@expo/vector-icons/AntDesign";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 import { Tabs } from "expo-router";
 import React from "react";
+import { SafeAreaView } from "react-native";
 
 const DashBoardLayout = () => {
   return (
-    <Tabs>
-      <Tabs.Screen
-        name="home"
-        options={{
-          title: "Home",
-          tabBarIcon: (data) => (
-            <AntDesign name="home" size={24} color="black" />
-          ),
+    <SafeAreaView className="flex-1 bg-white">
+      <Tabs
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: "#007AFF",
+          tabBarInactiveTintColor: "#8E8E93",
+          tabBarStyle: {
+            backgroundColor: "#FFFFFF",
+            borderTopWidth: 0,
+            elevation: 0,
+          }
         }}
-      />
-      <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-          tabBarIcon: (data) => (
-            <AntDesign name="profile" size={24} color="black" />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="setting"
-        options={{
-          title: "Settings",
-          tabBarIcon: (data) => (
-            <AntDesign name="setting" size={24} color="black" />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="tasks/index"
-        options={{
-          title: "Tasks",
-          tabBarIcon: (data) => (
-            <FontAwesome5 name="tasks" size={24} color="black" />
-          ),
-        }}
-      />
-    </Tabs>
+      >
+        <Tabs.Screen
+          name="home"
+          options={{
+            title: "Home",
+            tabBarIcon: (data) => (
+              <AntDesign name="home" size={24} color="black" />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            title: "Profile",
+            tabBarIcon: (data) => (
+              <AntDesign name="profile" size={24} color="black" />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="setting"
+          options={{
+            title: "Settings",
+            tabBarIcon: (data) => (
+              <AntDesign name="setting" size={24} color="black" />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="tasks/index"
+          options={{
+            title: "Tasks",
+            tabBarIcon: (data) => (
+              <FontAwesome5 name="tasks" size={24} color="black" />
+            ),
+          }}
+        />
+      </Tabs>
+    </SafeAreaView>
   );
 };
 
