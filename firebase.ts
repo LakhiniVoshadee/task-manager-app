c2358459e50e3084f323c5ba4d7e5bf025496f51
Lakhini Voshadee Abeysundara <lakhinivoshadee24@gmail.com>
2025-08-15 14:00:44 +0530

    created authservice

--- firebase.ts
+++ firebase.ts
@@ -3,6 +3,8 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import { getAuth } from "firebase/auth";
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCwUE5SOGfNe2HvE5WzydLx__7xMc2PvFk",
@@ -10,8 +12,10 @@ const firebaseConfig = {
   projectId: "task-manager-c6d73",
   storageBucket: "task-manager-c6d73.firebasestorage.app",
   messagingSenderId: "1080290203348",
-  appId: "1:1080290203348:web:2bc7d3487b68179af0cd41"
+  appId: "1:1080290203348:web:2bc7d3487b68179af0cd41",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
